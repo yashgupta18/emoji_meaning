2d4cf70cc682ceadce5be4a6bc3b276f536a040d
yashgupta18 <47364358+yashgupta18@users.noreply.github.com>
2020-12-17 15:28:33 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,47 @@
+import React, { useState } from "react";
+import "./styles.css";
+
+var dict = {
+  "😂": "happy",
+  "😞": "sad",
+  "😡": "angry",
+  "❤️": "heart",
+  "😖": "annoyed"
+};
+
+var emojiKnown = Object.keys(dict);
+// console.log(emojiKnown)
+
+export default function App() {
+  var [userInput, setUserInput] = useState("");
+  var [emojiMeaning, setMeaning] = useState("Select a emoji");
+
+  function inputChangeHandler(event) {
+    var meaning = dict[event.target.value];
+    if (meaning === undefined) {
+      meaning = "We don't have meaning of this emoji";
+    }
+    setMeaning(meaning);
+  }
+
+  function clickHandler(emoji) {
+    var meaning = dict[emoji];
+    setMeaning(meaning);
+  }
+
+  return (
+    <div className="App">
+      <h1>WELCOME</h1>
+      <input onChange={inputChangeHandler} />
+      <div>{emojiMeaning}</div>
+      <h3>Dictionary</h3>
+      {emojiKnown.map(function (emoji) {
+        return (
+          <span onClick={() => clickHandler(emoji)} key={emoji}>
+            {emoji}
+          </span>
+        );
+      })}
+    </div>
+  );
+}
